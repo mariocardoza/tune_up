e21102d75ffde73a46100fcd233825b1c7c40a9a
Mario Cardoza <mariokr.rocker@gmail.com>
2020-05-15 15:14:07 -0600

    Nuevo commit5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,8 @@
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
+              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item active">Panel de control</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
