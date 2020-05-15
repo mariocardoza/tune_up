413fd654af9cf8ba851d77f8c70e9d346d498468
Mario Cardoza <mariokr.rocker@gmail.com>
2020-05-15 16:33:21 -0600

    scrips sql2

---
+++ scripts/marcas.sql
@@ -0,0 +1,79 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50726
+Source Host           : localhost:3306
+Source Database       : taller
+
+Target Server Type    : MYSQL
+Target Server Version : 50726
+File Encoding         : 65001
+
+Date: 2020-05-15 16:32:30
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+
+-- ----------------------------
+-- Records of marcas
+-- ----------------------------
+INSERT INTO `marcas` VALUES ('1', 'AUDI', '1', null, null);
+INSERT INTO `marcas` VALUES ('2', 'BMW', '1', null, null);
+INSERT INTO `marcas` VALUES ('3', 'CHEVROLET', '1', null, null);
+INSERT INTO `marcas` VALUES ('4', 'CHRYSLER', '1', null, null);
+INSERT INTO `marcas` VALUES ('5', 'CLARK', '1', null, null);
+INSERT INTO `marcas` VALUES ('6', 'DAEWOO', '1', null, null);
+INSERT INTO `marcas` VALUES ('7', 'DAIHATSU', '1', null, null);
+INSERT INTO `marcas` VALUES ('8', 'DATSUN', '1', null, null);
+INSERT INTO `marcas` VALUES ('9', 'DODGE', '1', null, null);
+INSERT INTO `marcas` VALUES ('10', 'EVINRUDE', '1', null, null);
+INSERT INTO `marcas` VALUES ('11', 'EZGO', '1', null, null);
+INSERT INTO `marcas` VALUES ('12', 'FORD', '1', null, null);
+INSERT INTO `marcas` VALUES ('13', 'FREIGHTLINER', '1', null, null);
+INSERT INTO `marcas` VALUES ('14', 'GEO', '1', null, null);
+INSERT INTO `marcas` VALUES ('15', 'GLASTRON', '1', null, null);
+INSERT INTO `marcas` VALUES ('16', 'GOLF', '1', null, null);
+INSERT INTO `marcas` VALUES ('17', 'HONDA', '1', null, null);
+INSERT INTO `marcas` VALUES ('18', 'HYUNDAI', '1', null, null);
+INSERT INTO `marcas` VALUES ('19', 'INTERNATIONAL', '1', null, null);
+INSERT INTO `marcas` VALUES ('20', 'ISUZU', '1', null, null);
+INSERT INTO `marcas` VALUES ('21', 'JEEP', '1', null, null);
+INSERT INTO `marcas` VALUES ('22', 'JOHSON', '1', null, null);
+INSERT INTO `marcas` VALUES ('23', 'JONHSON', '1', null, null);
+INSERT INTO `marcas` VALUES ('24', 'KIA', '1', null, null);
+INSERT INTO `marcas` VALUES ('25', 'L M V', '1', null, null);
+INSERT INTO `marcas` VALUES ('26', 'LANCHA', '1', null, null);
+INSERT INTO `marcas` VALUES ('27', 'LAND ROVER', '1', null, null);
+INSERT INTO `marcas` VALUES ('28', 'LEXUS', '1', null, null);
+INSERT INTO `marcas` VALUES ('29', 'MASEY FERGUSON', '1', null, null);
+INSERT INTO `marcas` VALUES ('30', 'MAZDA', '1', null, null);
+INSERT INTO `marcas` VALUES ('31', 'MERCEDES BENZ', '1', null, null);
+INSERT INTO `marcas` VALUES ('32', 'MERCURY', '1', null, null);
+INSERT INTO `marcas` VALUES ('33', 'MINI', '1', null, null);
+INSERT INTO `marcas` VALUES ('34', 'MITSUBISHI', '1', null, null);
+INSERT INTO `marcas` VALUES ('35', 'NISSAN', '1', null, null);
+INSERT INTO `marcas` VALUES ('36', 'PEUGEOT', '1', null, null);
+INSERT INTO `marcas` VALUES ('37', 'PONTIAC', '1', null, null);
+INSERT INTO `marcas` VALUES ('38', 'RENAULT', '1', null, null);
+INSERT INTO `marcas` VALUES ('39', 'REXTON', '1', null, null);
+INSERT INTO `marcas` VALUES ('40', 'SALV', '1', null, null);
+INSERT INTO `marcas` VALUES ('41', 'SCION', '1', null, null);
+INSERT INTO `marcas` VALUES ('42', 'SEAT', '1', null, null);
+INSERT INTO `marcas` VALUES ('43', 'SIMÁN', '1', null, null);
+INSERT INTO `marcas` VALUES ('44', 'SMART', '1', null, null);
+INSERT INTO `marcas` VALUES ('45', 'SSANGYONG', '1', null, null);
+INSERT INTO `marcas` VALUES ('46', 'SUBARU', '1', null, null);
+INSERT INTO `marcas` VALUES ('47', 'SUZUKI', '1', null, null);
+INSERT INTO `marcas` VALUES ('48', 'TIGER', '1', null, null);
+INSERT INTO `marcas` VALUES ('49', 'TOYOTA', '1', null, null);
+INSERT INTO `marcas` VALUES ('50', 'TOYOTA ECHO', '1', null, null);
+INSERT INTO `marcas` VALUES ('51', 'V W', '1', null, null);
+INSERT INTO `marcas` VALUES ('52', 'VOLKSWAGEN', '1', null, null);
+INSERT INTO `marcas` VALUES ('53', 'VOLSWAGEN', '1', null, null);
+INSERT INTO `marcas` VALUES ('54', 'VOLVO', '1', null, null);
+INSERT INTO `marcas` VALUES ('55', 'VW', '1', null, null);
+INSERT INTO `marcas` VALUES ('56', 'YALE', '1', null, null);
+INSERT INTO `marcas` VALUES ('57', 'YAMAHA', '1', null, null);
+SET FOREIGN_KEY_CHECKS=1;
