66f21aec08579323952300d9716e7d5fa04e260b
Mario Cardoza <mariokr.rocker@gmail.com>
2021-02-25 15:50:59 -0600

    [CHANGED] * seeder contribuyente

--- public/js/app.js
+++ public/js/app.js
@@ -74982,8 +74982,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\taller_t\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\taller_t\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/mario/tune_up/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/mario/tune_up/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
